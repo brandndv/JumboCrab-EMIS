eebdce6b17f55be2327e5f713d57b743ce66e858
donny.dev <brandonlamagna111@gmail.com>
2025-11-02 22:19:09 +0800

    Temporary commit before switching branches

---
+++ src/app/(users)/(admin dashboard)/admin/(nav)/employees/page.tsx
@@ -0,0 +1,11 @@
+import ManageEmployees from "@/components/dasboard/manage-empoyees/employees-provider";
+
+const EmployeesPage = () => {
+  return (
+    <div className="px-20 py-10">
+      <ManageEmployees />
+    </div>
+  );
+};
+
+export default EmployeesPage;
