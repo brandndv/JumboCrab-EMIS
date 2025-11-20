ef3d3c9d821a0cc633b7c1209df8a5227ab210e6
Donny.dev <brandonlamagna111@gmail.com>
2025-11-20 20:25:51 +0800

    next-theme-init

--- src/app/(users)/(admin)/admin/(nav)/deduction/1/2/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Deduction2Page = () => {
-  return (
-    <div>Deduction2Page</div>
-  )
-}
-
-export default Deduction2Page
--- src/app/(users)/(admin)/admin/(nav)/deduction/1/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Deduction1Page = () => {
-  return (
-    <div>Deduction1Page</div>
-  )
-}
-
-export default 1
